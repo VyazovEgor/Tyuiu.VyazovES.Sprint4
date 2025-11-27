957cc86886581eba6ad2b5973081177f16b22f29
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-28 01:11:53 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint4.Task2.V9/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VyazovES.Sprint4.Task2.V9.Lib;
+
+DataService ds = new DataService();
+int s;
+int len;
+len = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[len];
+
+
+for (int i = 0; i <= len; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+s = ds.Calculate(a);
+
+
+Console.WriteLine(s);
+
+
+Console.ReadKey();
