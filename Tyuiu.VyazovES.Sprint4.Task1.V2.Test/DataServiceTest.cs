d868a164d210b5923b50de84fe81b5cc575e7693
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-28 00:53:44 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint4.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.VyazovES.Sprint4.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
