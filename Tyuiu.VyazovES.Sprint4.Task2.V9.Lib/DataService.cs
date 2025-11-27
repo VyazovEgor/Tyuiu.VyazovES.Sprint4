d3b029e5440fea90c681871f36648dc6018f8824
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-28 01:13:35 +0500

    1

--- Tyuiu.VyazovES.Sprint4.Task2.V9.Lib/DataService.cs
+++ Tyuiu.VyazovES.Sprint4.Task2.V9.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.VyazovES.Sprint4.Task2.V9.Lib
     {
         public int Calculate(int[] array)
         {
-            int sum = 0;
+            int sum = 1;
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
