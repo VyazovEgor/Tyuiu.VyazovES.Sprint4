b6148532e92e0bf7d279240504dee50396f5d6d5
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-29 00:41:04 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint4.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VyazovES.Sprint4.Task7.V11.Lib
+{
+    public class DataService : ISprint4Task7V11
+    {
+        public int Calculate(int rows, int cols, string str)
+        {
+            int Count = 0;
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(str[i * cols + j].ToString());
+                    if (matrix[i, j] % 2 != 0)
+                        Count++;
+                }
+            }
+            return Count;
+        }
+    }
+}
+
---
+++ Tyuiu.VyazovES.Sprint4.Task7.V11/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VyazovES.Sprint4.Task7.V11.Lib;
+
+DataService ds = new DataService();
+string str = "56789012";
+int rows = 4, cols = 2;
+int a = ds.Calculate(rows, cols, str);
+
+
+Console.WriteLine(a);
+
+
+Console.ReadKey();
