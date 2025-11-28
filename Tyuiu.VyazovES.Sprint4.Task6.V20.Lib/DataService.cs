96f86f7a2ba071ce078e45785ea2bfa7e3c95fea
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-29 00:19:06 +0500

    1

---
+++ Tyuiu.VyazovES.Sprint4.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VyazovES.Sprint4.Task6.V20.Lib
+{
+    public class DataService : ISprint4Task6V20
+    {
+        public string[] Calculate(string[] array)
+        {
+            List<string> resultList = new List<string>();
+
+            Array.ForEach(array, subject =>
+            {
+                if (subject.Length < 10)
+                    resultList.Add(subject);
+            });
+
+            return resultList.ToArray();
+        }
+    }
+}
+
---
+++ Tyuiu.VyazovES.Sprint4.Task6.V20/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VyazovES.Sprint4.Task6.V20.Lib;
+
+DataService ds = new DataService();
+
+string[] subjects = { "Математика", "Физика", "Химия", "Биология", "География", "История", "Литература" };
+subjects = ds.Calculate(subjects);
+
+
+Console.WriteLine(s);
+
+
+Console.ReadKey();
